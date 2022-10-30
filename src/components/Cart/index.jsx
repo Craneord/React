527983b7ed0c819095ae8c412f4e5ea8739dbe5b
pablo pallas <pallaspablo92@gmail.com>
2022-10-30 16:11:57 -0300

    Utilizando post

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -3,9 +3,27 @@ import "./cart.css"
 import { useCartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 import ItemCart from "../itemCart";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const Cart = () => {
     const {cart, totalPrice} = useCartContext()
+
+    const order ={
+        comprador:{
+            name: "pablo",
+            email:"pallaspablo@gmail",
+            phone:343423434,
+            address:"lituania"
+        },
+        items: cart.map(product => ({id : product.id, title: product.title, price : product.price, quantity: product.quantity})),
+        total: totalPrice() ,
+    }
+    const handleClick = () => {
+        const db= getFirestore();
+        const ordersCollection = collection (db, "orders");
+        addDoc(ordersCollection, order)
+        .then(({ id }) => console.log(id))
+    }
     
     if (cart.length === 0){
     
@@ -25,6 +43,7 @@ const Cart = () => {
             <p>
                 TOTAL: {totalPrice()}
             </p>
+            <><button onClick={handleClick}>Comprar</button></>
         </>
     )
 }
